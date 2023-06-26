44bdb96e825e0d5a59dd3ce604dc83a205e98912
Tejal Salve <60121669+tejalsalve19@users.noreply.github.com>
2023-06-26 15:36:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Animal {}
+class Animal 
+{
+	constructor(species) 
+	{
+	    this._species = species;
+	}
+	get species() 
+	{
+	    return this._species;
+	}
+	makeSound() 
+	{
+	    console.log("The animal makes a sound.");
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	bark() {
+    console.log("woof");
+  }
+}
 
-class Cat extends Animal {}
+class Cat extends Animal 
+{
+	purr() 
+	{
+	    console.log("purr");
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
